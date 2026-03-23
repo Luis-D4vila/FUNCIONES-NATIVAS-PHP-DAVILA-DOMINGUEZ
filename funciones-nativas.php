cfdce1215f7b777f52b00f5e93b1b4436ceb3e8a
Luis Anderson Davila Dominguez <andersondavila59@gmail.com>
2026-03-23 13:35:05 -0600

    array_key_exists()

--- funciones-nativas.php
+++ funciones-nativas.php
@@ -10,4 +10,7 @@ $personas = [
 ];
 $edades = array_column($personas,"edad"); // [25,30]
 
+// 2. array_key_exists() -> verifica si existe una clave dentro de nuestro programa,  cuando quieras asegurarte de que una clave está definida en el array, sin importar si su valor es null.
+if(array_key_exists("nombre",$personas[0])){ echo "Sí existe"; }
+
 
